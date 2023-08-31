51fedce8a8f8fd3141920f6e7c400db135da77ad
lokeshkumar111 <lokeshk.charan3@gmail.com>
2023-08-31 16:38:02 +0530

    done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,9 +4,9 @@ import {Link} from 'react-router-dom';
 const Home = () => {
   return (
     <div className={styles.homeContainer}>
-        <h1>Hello, Welcome to the MyTodo Appllication</h1>
-        <h4>Here, You can find the best way to manage your tasks</h4>
-        <p>Let's Start your Todo journey click on the below button or add your first task</p>
+        <h1>Hello, Welcome to MyTodo Appllication</h1>
+        <h4>Here, You will find the best way to manage your tasks</h4>
+        <p>Let's Start your Todo journey just click on the below button to add your first todo task</p>
         <Link to="/addtodo"><button>Add Todo</button></Link>
     </div>
   )
